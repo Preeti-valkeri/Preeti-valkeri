28d6459d1c96b5fba4e25fb02b654f24bb8abaad
Preeti-valkeri <77795092+Preeti-valkeri@users.noreply.github.com>
2024-10-14 17:48:41 +0400

    Angular basics and sample project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
+import { appConfig } from './app/app.config';
+
+bootstrapApplication(AppComponent,
+  {
+    providers: [
+      provideProtractorTestingSupport(),
+      provideRouter(routeConfig)
+    ]
+  }
+).catch(err => console.error(err));
